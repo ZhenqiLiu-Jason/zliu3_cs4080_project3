56c4c44197ac0606784c56f5c8fae05b90089893
Jason Liu <zhenqiliu.jason@gmail.com>
2025-05-10 22:57:30 -0600

    Updated self-loop removel process in karger algorithm

--- sources/karger_algo.py
+++ sources/karger_algo.py
@@ -11,19 +11,24 @@ def karger_min_cut(original_graph: nx.Graph) -> int:
         u, v = random.choice(list(G.edges()))
 
         # 2. Contract node v into u
-        for neighbor in list(G.neighbors(v)):
-            # Add all edges from v to its neighbors (including u)
+        # Ignore u in v's neighbors to avoid self loops
+        for neighbor in (n for n in G.neighbors(v) if n != u):
             G.add_edges_from([(u, neighbor)] * G.number_of_edges(v, neighbor))
 
-        # 3. Remove the contracted node and self-loops
+        # 3. Remove the contracted node
         G.remove_node(v)
-        G.remove_edges_from(list(nx.selfloop_edges(G)))
 
     # Remaining edges between the 2 supernodes = cut size
     return G.number_of_edges()
 
 
 def repeated_karger_min_cut(G: nx.Graph, trials: int = 100) -> int:
+    """
+    Repeatedly run Karger's algorithm to find the min-cut of the graph.
+
+    Keeps track of the minimum min-cut of all runs.
+    """
+
     min_cut = float('inf')
     for _ in range(trials):
         cut = karger_min_cut(G)
--- sources/main.py
+++ sources/main.py
@@ -4,7 +4,7 @@ from get_graph import *
 from karger_algo import *
 
 
-G = get_connected_graph(100)
+G = get_connected_graph(50)
 
 print("Edges:", list(G.edges))
 print("Is connected?", nx.is_connected(G))
@@ -13,4 +13,4 @@ print("Number of edges:", G.number_of_edges())
 cut_value, partition = nx.stoer_wagner(G)
 print(cut_value)
 
-print("Estimated min-cut:", repeated_karger_min_cut(G, trials=15))
+print("Estimated min-cut:", repeated_karger_min_cut(G, trials=50))
